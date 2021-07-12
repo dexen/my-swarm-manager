af10296efc8102d0221297b96f19d1afed5c73d7
dexen deVries <dexen.devries@gmail.com>
2021-07-12 18:02:38 +0200

    swarm-2: let's try a new approach

---
+++ router.php
@@ -0,0 +1,15 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', true);
+
+function renderListing()
+{
+	echo '<!DOCTYPE html>';
+	echo '<html>';
+	echo '<body>';
+
+	phpinfo();
+}
+
+renderListing();
